618c18c8f3a51410c5ec0e848df694510f96950b
catalin128 <97623661+catalin128@users.noreply.github.com>
2022-11-01 17:07:48 +0200

    Add files via upload

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,2 +1,47 @@
-public class MainWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow extends JFrame {
+
+    private JPanel panouPrincipal;
+    private JButton btnJocuri;
+    private JButton btnClienti;
+    private JButton btnMagazin;
+    private JLabel labelPrincipal;
+    private JLabel labelBackground=new JLabel();
+    private JFrame frame_joc;
+
+    public MainWindow()
+    {
+        panouPrincipal.setOpaque(false);
+       // panouPrincipal.setLayout(new FlowLayout());
+        labelBackground.setLayout(new FlowLayout());
+        labelBackground.setIcon(new ImageIcon("C:\\Users\\Asus TUF\\Desktop\\POO\\ProiectFinal-main\\src\\fundal1.jpg"));
+        labelBackground.setLayout(new BorderLayout());
+        labelBackground.add(panouPrincipal);
+        add(labelBackground);
+
+        btnJocuri.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame_joc = new FereastraJoc();
+                frame_joc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame_joc.pack();
+                //frame.setLocationRelativeTo(null);
+                frame_joc.setVisible(true);
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame=new MainWindow();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        //frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
 }
